3f730624e9b4dac0725c58d73957e1fcc42d323b
Alex <alex@Alexs-iMac.local>
2015-06-30 05:14:38 +0300

    Initial Commit

---
+++ AVA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVA
+//
+//  Created by Alex on 30/06/15.
+//  Copyright (c) 2015 Jeims. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
